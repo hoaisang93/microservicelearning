44ea3f15060d2d8936894e8729c9101094bd5a6b
sanglh <hapandpy@gmail.com>
2020-04-18 11:59:05 +0700

    first commit simple .net core app

---
+++ StartUp.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+namespace microservicelearning
+{
+    public class StartUp
+    {
+        public StartUp(IHostingEnvironment env)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("hello world");
+            });
+        }
+    }
+}
